e3c4184b0fb06083bbfc0d385e343e9f46fa6ece
Aly-exe <alibnraslan@gmail.com>
2026-06-07 04:19:37 +0200

    Add Setting Screen Icons & Start execute Ui for this screen

--- lib/features/user/presentation/views/SettingsScreen.dart
+++ lib/features/user/presentation/views/SettingsScreen.dart
@@ -1,11 +1,40 @@
 
+import 'package:agro/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Settings Screen"),);
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: Stack(
+            children: [
+              CircleAvatar(
+                radius: 85,
+                backgroundColor: AppColors.imageContainerBackgroundColor,
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage("assets/images/Ali.jpeg"),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: AppColors.lightPrimaryColor,
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                child: SvgPicture.asset("assets/icons/editicon.svg"),
+              )
+            ],
+          )
+        )
+      ],
+    );
   }
 }
